f2857afb3b61bf8dd63eb4febf2379aa4cccc30a
Chad Mcpeek <cmcpeek17@gmail.com>
2024-03-31 14:20:36 -0700

    assignment 2

---
+++ finder-app/writer.c
@@ -0,0 +1,43 @@
+/* 1. Check the amount of inputs. If != 2 -> exit
+ * 2. If the directory for the file is not created, then create it.
+ * 3. Try creating the file. If the file could not be created then exit with a 1
+ * */
+
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main ( int argc, char* argv[] ) {
+
+	int fd;
+
+	openlog(NULL, 0, LOG_USER);
+	if (argc != 3) {
+		syslog(LOG_ERR, "wrong amount of inputs provided: %d", argc);
+		return 1;
+	}
+
+	fd = creat (argv[2], 0664);
+	if (fd == -1) {
+		syslog(LOG_ERR, "file could not be created");
+		return 1;
+	}
+	
+	ssize_t write_size;
+	syslog(LOG_ERR, "Writing %s to %s\n", argv[2], argv[1]);
+	write_size = write( fd, argv[1], strlen(argv[1]));
+	
+	if (write_size == -1) {
+		syslog(LOG_ERR, "file could not be written");
+		return 1;
+	}
+
+	if (close(fd) == -1) {
+		syslog(LOG_ERR, "file could not be closed");
+		return 1;
+	} else {
+		return 0;
+	}
+
+}
